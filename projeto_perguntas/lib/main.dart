df1501d2d4be264e4e25978a2aacbcb77fc60b43
ray396 <rayssadantas31@gmail.com>
2023-02-06 13:52:23 -0300

    estudo do flutter

---
+++ projeto_perguntas/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Olá Flutter!!!'),
+      ),
+    );
+  }
+}
