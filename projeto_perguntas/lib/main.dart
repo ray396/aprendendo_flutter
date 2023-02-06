7ca23563fa4e1d3ab3ccee07a3c00547213249f8
ray396 <rayssadantas31@gmail.com>
2023-02-06 15:51:27 -0300

    estudo do flutter

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -3,16 +3,51 @@ import 'package:flutter/widgets.dart';
 
 main() => runApp(PerguntaApp());
 
-class PerguntaApp extends StatelessWidget {
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    perguntaSelecionada++;
+    print(perguntaSelecionada);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter!!!'),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: () {
+                print('Resposta 2 foi selecionada!');
+              },
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: () => print('Resposta 3!!!'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
